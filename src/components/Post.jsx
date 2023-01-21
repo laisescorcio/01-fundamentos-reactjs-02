679f8bbaa88218537c17fd476cedc0ae0a754909
laisescorcio <lais.correia@outlook.com>
2023-01-21 20:05:07 -0300

    feat: Post - insert publication and textarea

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -20,20 +20,30 @@ export function Post(props) {
 
             <div className={styles.content}>
                 <p>Fala galeraa 👋</p>
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p> 👉 <a href="">jane.design/doctorcare</a></p>
+                <p>Acabei de subir mais um projeto no meu portifa. 
+                    É um projeto que fiz no NLW Return, evento da Rocketseat. 
+                    O nome do projeto é DoctorCare 🚀
+                </p>
                 <p>
-                    <a href="">#novoprojeto</a>
-                    <a href="">#nlw</a>
-                    <a href="">#rocketseat </a>
+                    <a href="">jane.design/doctorcare</a>
+                </p>
+                <p>
+                    <a href="">#novoprojeto</a>{' '}
+                    <a href="">#nlw</a>{' '}
+                    <a href="">#rocketseat</a>
                 </p>
             </div>
 
-            {/* <form className={styles.commentForm}>
+            <form className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
                 
-                <textarea />
-            </form> */}
+                <textarea 
+                    placeholder="Deixe um comentário"
+                />
+
+                <button type="submit">Publicar</button>
+
+            </form>
         </article>
     )
 }
