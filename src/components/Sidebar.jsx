5cc5a82eb922aa044674966235f506d5a656cf5a
laisescorcio <lais.correia@outlook.com>
2023-01-21 17:37:54 -0300

    style: Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import styles from './Sidebar.module.css'
+import { PencilLine } from 'phosphor-react'
 
 export function Sidebar() {
     return (
@@ -8,12 +9,14 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1524850011238-e3d235c7d4c9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40"
             />
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/61030205?v=4" />
                 <strong>Laís Escorcio</strong>
                 <span>Tech Lead</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20}/>
                     Editar seu Perfil
                 </a>
             </footer>
