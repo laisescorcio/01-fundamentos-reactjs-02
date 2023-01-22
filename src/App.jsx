24d85967b0d8a9cf6f6e4c800e8e76be700120c0
laisescorcio <lais.correia@outlook.com>
2023-01-21 21:55:59 -0300

    feat: Post - map to render the publications

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const posts = [
     content: [
       { type: 'paragraph', content: 'Fala galeraa 👋'},
       { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
-      { type: 'paragraph', content: 'jane.design/doctorcare'}
+      { type: 'link', content: 'jane.design/doctorcare'}
     ],
     publishedAt: new Date('2023-01-21 20:00:00')
   },
@@ -33,7 +33,7 @@ const posts = [
     content: [
       { type: 'paragraph', content: 'Fala galeraa 👋'},
       { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
-      { type: 'paragraph', content: 'jane.design/doctorcare'}
+      { type: 'link', content: 'jane.design/doctorcare'}
     ],
     publishedAt: new Date('2023-01-21 20:00:00')
   }
